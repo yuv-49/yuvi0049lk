6ed63767160bfb1e77cff2a76adbf9c1c66cfbd9
yuvi0049 <yuvi0049@gmail.com>
2017-03-29 15:37:09 +0530

    Initial Commit

---
+++ Leuk_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Leuk_iOS
+//
+//  Created by yuvraj singh on 29/03/17.
+//  Copyright © 2017 yuvi0049. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
