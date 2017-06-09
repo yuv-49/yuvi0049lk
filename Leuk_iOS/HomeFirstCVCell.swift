5292aeda7bc17217eec8c04d965e9f967cd406bf
yuvi0049 <yuvi0049@gmail.com>
2017-06-09 06:56:40 +0530

    commit final

--- Leuk_iOS/HomeFirstCVCell.swift
+++ Leuk_iOS/HomeFirstCVCell.swift
@@ -20,8 +20,53 @@ class HomeFirstCVCell: UICollectionViewCell {
 		super.awakeFromNib()
 		
 		let view = UIView(frame: frame)
-		view.layer.borderWidth = 1
-		view.layer.borderColor = UIColor.black.cgColor
+//		view.layer.shadowColor = UIColor.gray.cgColor
+//		//view.layer.shadowPath = UIBezierPath(roundedRect:view.bounds, cornerRadius:view.contentView.layer.cornerRadius).cgPath
+//		view.layer.shadowOffset = CGSize(width: 0, height: 2.0)
+//		view.layer.shadowRadius = 2.0
+//
+//		view.layer.masksToBounds = false
+//		view.layer.shadowOpacity = 1.0
+//		
+
+//		view.layer.shadowColor = UIColor.black.cgColor
+//		view.layer.shadowOpacity = 1
+//		view.layer.shadowOffset = CGSize.zero
+//		view.layer.shadowRadius = 10
+//		
+//		view.layer.shouldRasterize = true
+
+		
+		
+		
+		
+//		view.layer.borderWidth = 2
+//		view.layer.borderColor = UIColor.lightGray.cgColor
+		self.addSubview(view)
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		cell.contentView.layer.cornerRadius = 10
+//		cell.contentView.layer.borderWidth = 1.0
+//		
+//		cell.contentView.layer.borderColor = UIColor.clear.cgColor
+//		cell.contentView.layer.masksToBounds = true
+//		
+//		cell.layer.shadowColor = UIColor.gray.cgColor
+//		cell.layer.shadowOffset = CGSize(width: 0, height: 2.0)
+//		cell.layer.shadowRadius = 2.0
+//		cell.layer.shadowOpacity = 1.0
+//		cell.layer.masksToBounds = false
+//		cell.layer.shadowPath = UIBezierPath(roundedRect:cell.bounds, cornerRadius:cell.contentView.layer.cornerRadius).cgPath
+		
+		
+		
 	}
 	
 	
--- Leuk_iOS/OrderVCell.swift
+++ Leuk_iOS/OrderVCell.swift
@@ -9,5 +9,28 @@
 import UIKit
 
 class OrderVCell: UICollectionViewCell {
-    
+	
+	
+	
+	@IBOutlet weak var viewToColor: UIView!
+	@IBOutlet weak var placeImage: UIImageView!
+	@IBOutlet weak var orderDate: UILabel!
+	@IBOutlet weak var orderTime: UILabel!
+	@IBOutlet weak var placeName: UILabel!
+	@IBOutlet weak var items: UILabel!
+	@IBOutlet weak var totalCost: UILabel!
+	@IBOutlet weak var orderStatus: UILabel!
+	
+	
+	
+	
+	override func awakeFromNib() {
+		super.awakeFromNib()
+		
+		let view = UIView(frame: frame)
+		view.layer.borderWidth = 2
+		view.layer.borderColor = UIColor.black.cgColor
+	}
+	
+	
 }
